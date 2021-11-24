fd604c8b9c9d6f60dc76f0fede9d7cd2df641565
Damishon <92202933+Damishon@users.noreply.github.com>
2021-11-24 11:48:16 +0500

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -3,13 +3,18 @@ fhand = open ("mailbox.txt")
 lines=fhand.readlines()
 Damishon=[]
 file2=open('output.txt', 'w')
+
 for line in lines:
    if 'SMTP ID' in line:
-      if line not in Damishon:
-         Damishon.append(line[68:-1])
-Damishon.sort()
+     print(line[68:-1])
+     if line not in Damishon:
 
-print(Damishon)
+           Damishon.append(line[68:-1])
+   Damishon.sort()
+
+if line in Damishon:
+     file2.write(line[68:-1])
+     file2.write('\n')
 
 file2.write(str(Damishon)) 
 file2.close()

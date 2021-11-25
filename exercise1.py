723af4750dc1cc31b37d2ba74d9458b594a9a857
Damishon <92202933+Damishon@users.noreply.github.com>
2021-11-25 12:36:42 +0500

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -7,18 +7,22 @@ file2=open('output.txt', 'w')
 for line in lines:
    if 'SMTP ID' in line:
      print(line[68:-1])
-     if line not in Damishon:
+     if line[68:-1] not in Damishon:
+        Damishon.append(line[68:-1])
 
-           Damishon.append(line[68:-1])
-   Damishon.sort()
+     Damishon.sort()
+#if line[68:-1] in Damishon:
+     #file2.write(line[68:-1])
+     #file2.write('/n')
+for item in Damishon:
+     file2.write(item)
+     file2.write('/n')
 
-if line in Damishon:
-     file2.write(line[68:-1])
-     file2.write('\n')
 
-file2.write(str(Damishon)) 
 file2.close()
 fhand.close()
+
+
 
 
 
